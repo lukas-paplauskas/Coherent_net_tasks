d6258611e4300cc71349d21f40ebf0efe931c5df
lukas-paplauskas <lukassas1@gmail.com>
2022-01-17 18:03:05 +0200

    Started creating DiagonalMatrix class, created constructor and Track method

---
+++ CoherentTasks/CoherentTasks/DiagonalMatrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoherentTasks
+{
+    public class DiagonalMatrix
+    {
+        public int[] MatrixDiagonalElements { get; set; }
+        public readonly int Size;
+
+        public DiagonalMatrix(params int[] diagonalElements)
+        {
+            MatrixDiagonalElements = diagonalElements;
+            Size = diagonalElements?.Length <= 0 ? 0 : diagonalElements.Length;
+        }
+
+        public int Track()
+        {
+            return MatrixDiagonalElements.Sum();
+        }
+
+        public override string ToString()
+        {
+            return " ";
+        }
+        //public override bool Equals(int[] otherMatrix) => MatrixDiagonalElements == otherMatrix;
+    }
+}
